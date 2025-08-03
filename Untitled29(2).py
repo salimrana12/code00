42189f8809c05727d9e572c1d5bc54e645306f14
Md. Salim Rana <salimmi29900@gmail.com>
2025-08-03 20:08:57 +0600

    rt44

--- Untitled29(2).py
+++ Untitled29(2).py
@@ -3,56 +3,25 @@
 
 # In[2]:
 
-
 import pandas as pd
-
-
-# In[3]:
-
-
+ In[3]:
 city = ['texus', 'clafornia', 'hollywood']
 df = pd.DataFrame(city)
 df
 type(city)
-
-
 # In[4]:
-
-
 city1 = ['europe', 'astraliya', 'north koria']
-
-
 # In[5]:
-
-
 city2 = ['bangladesh', 'singapur', 'london']
-
-
 # In[6]:
-
-
 df = pd.DataFrame(city)
-
-
 # In[7]:
-
-
 df
-
-
 # In[9]:
-
-
 df = pd.DataFrame(zip(city, city1, city2), columns=['city_in_usa', 'city_in_europe', 'city_in_asia'], index=['ci', 'c2', 'c3'])
-
-
-# In[10]:
-
-
+In[10]:
 df
-
-
-# In[ ]:
+In[ ]:
 
 
 
