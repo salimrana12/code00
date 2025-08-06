b9fe1555ce283889b638c3391c23a33615513e09
Md. Salim Rana <salimmi29900@gmail.com>
2025-08-06 18:33:09 +0600

    code22 is python file

---
+++ code22.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+city = ['texus', 'clafornia', 'hollywood']
+
+
+# In[4]:
+
+
+city1 = ['europe', 'astraliya', 'north koria']
+
+
+# In[5]:
+
+
+city2 = ['bangladesh', 'singapur', 'london']
+
+
+# In[6]:
+
+
+df = pd.DataFrame(city)
+
+
+# In[7]:
+
+
+df
+
+
+# In[9]:
+
+
+df = pd.DataFrame(zip(city, city1, city2), columns=['city_in_usa', 'city_in_europe', 'city_in_asia'], index=['ci', 'c2', 'c3'])
+
+
+# In[10]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
