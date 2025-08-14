f03ab867d4f60a23980713cff26294eb03b2eb11
salimrana12 <salimmi29900@gmail.com>
2025-08-14 22:26:42 +0600

    python code added

--- code22.py
+++ code22.py
@@ -1,56 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pandas as pd
-
-
-# In[3]:
-
 
 city = ['texus', 'clafornia', 'hollywood']
 
-
-# In[4]:
-
-
 city1 = ['europe', 'astraliya', 'north koria']
-
-
-# In[5]:
-
 
 city2 = ['bangladesh', 'singapur', 'london']
 
+df = pd.DataFrame(city1)
 
-# In[6]:
+print(df)
+df6 = pd.DataFrame(zip(city, city1, city2), columns=['city_in_usa', 'city_in_europe', 'city_in_asia'], index=['ci', 'c2', 'c3'])
 
-
-df = pd.DataFrame(city)
-
-
-# In[7]:
+print(df6)
 
 
-df
 
 
-# In[9]:
-
-
-df = pd.DataFrame(zip(city, city1, city2), columns=['city_in_usa', 'city_in_europe', 'city_in_asia'], index=['ci', 'c2', 'c3'])
-
-
-# In[10]:
-
-
-df
-df2 = ['telephone', 'older', 'pushfeatch']
-
-
-# In[ ]:
 
 
 
